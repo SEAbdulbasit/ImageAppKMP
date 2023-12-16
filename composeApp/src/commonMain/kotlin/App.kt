b13d8ba5335cd5f5370bd74f56e42a0c93dfe93d
Abdul Basit <abdulbasit5361234@gmail.com>
2023-12-16 14:05:38 +0500

    show link

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,39 +1,33 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import network.model.Hit
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     MaterialTheme {
-        var greetingText by remember { mutableStateOf("Hello World!") }
-        var showImage by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = {
-                greetingText = "Compose: ${Greeting().greet()}"
-                showImage = !showImage
-            }) {
-                Text(greetingText)
-            }
-            AnimatedVisibility(showImage) {
-                Image(
-                    painterResource("compose-multiplatform.xml"),
-                    null
-                )
-            }
+        val viewModel = ViewModel.getViewModel()
+        val state by viewModel.pixabayImages.collectAsState()
+        PicturesList(state?.hits?: emptyList())
+
+    }
+}
+
+
+@Composable
+fun PicturesList(hits: List<Hit?>) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(hits){
+            Text(modifier = Modifier.fillMaxWidth(), text = it?.largeImageURL!!)
         }
     }
 }
--- composeApp/src/commonMain/kotlin/network/model/Hit.kt
+++ composeApp/src/commonMain/kotlin/network/model/Hit.kt
@@ -1,6 +1,5 @@
 package network.model
 
-
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
